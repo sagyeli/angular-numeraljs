08edd62584dfaaa1d2d83fadef5436a1d94b00fc
Eli Sagy <sagyeli@gmail.com>
2014-07-03 18:51:12 +0300

    - A modification for emphasizing support

--- src/angular-numeraljs.js
+++ src/angular-numeraljs.js
@@ -1,13 +1,19 @@
 /*global numeral */
 'use strict';
 angular.module('ngNumeraljs', [])
-    .filter('numeraljs', function () {
+    .filter('numeraljs', ['$sce', function ($sce) {
         return function (input, format) {
             if (input == null || format == null) 
                 return input;
             if (format === '') 
                 return '';
-            
-            return numeral(input).format(format);
+
+            var nums = numeral(input).format(format).split('.');
+            nums[0] = '<span class="numeral-left">' + nums[0] + '</span>';
+            if (nums.length > 1) {
+            	nums[1] = '<span class="numeral-right">' + nums[1] + '</span>';
+            }
+
+            return $sce.trustAsHtml(nums.join('.'));
         };
-    });
+    }]);
