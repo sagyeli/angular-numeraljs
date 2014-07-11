e20030a62f1de6da9b22317b6658db775263554c
Eli Sagy <sagyeli@gmail.com>
2014-07-11 03:35:42 +0300

    Trimming zeros smartly

--- src/angular-numeraljs.js
+++ src/angular-numeraljs.js
@@ -8,7 +8,7 @@ angular.module('ngNumeraljs', [])
             if (format === '') 
                 return '';
 
-            var nums = numeral(input).format(format).split('.');
+            var nums = numeral(input).format(format).replace(/0+(?=\D+$)|^0+|0+$/g, '0').replace(/(^\D+|:)?0+/g, function(str, p1) {return p1 ? p1 + '0' : str;}).split('.');
             if (nums.length > 1) {
                 nums[0] = '<span class="numeral-left">' + nums[0] + '</span>';
             	nums[1] = '<span class="numeral-right">.' + nums[1] + '</span>';
