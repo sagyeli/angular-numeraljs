7b713c98f4c990ab25f33f8708454c4c3a4f7c64
Eli Sagy <sagyeli@gmail.com>
2014-07-16 01:01:33 +0300

    Regex bug fix

--- src/angular-numeraljs.js
+++ src/angular-numeraljs.js
@@ -8,13 +8,14 @@ angular.module('ngNumeraljs', [])
             if (format === '') 
                 return '';
 
-            var nums = numeral(input).format(format).replace(/0+(?=\D+$)|^0+|0+$/g, '0').replace(/(^\D+|:)?0+/g, function(str, p1) {return p1 ? p1 + '0' : str;}).split('.');
-            if (nums.length > 1) {
+            var nums = numeral(input).format(format).replace(/0+(?=\D+$)|0+$/g, '').replace(/(^\D*|:)?0+/g, function(str, p1) {return p1 ? p1 + '0' : str;}).split('.');
+            if (nums.length > 1 && parseInt(nums[1].replace('/\D/g', '')) > 0) {
                 nums[0] = '<span class="numeral-left">' + nums[0] + '</span>';
             	nums[1] = '<span class="numeral-right">.' + nums[1] + '</span>';
             }
             else {
                 nums[0] = '<span class="numeral-integer">' + nums[0] + '</span>';
+                nums = [nums[0]];
             }
 
             return $sce.trustAsHtml(nums.join(''));
