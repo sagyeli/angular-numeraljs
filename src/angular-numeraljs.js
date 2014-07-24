ec699f3986cd57f9e07826f90e5ef715ef24603b
Eli Sagy <sagyeli@gmail.com>
2014-07-25 02:12:09 +0300

    Adding no zeros trimming option

--- src/angular-numeraljs.js
+++ src/angular-numeraljs.js
@@ -2,7 +2,7 @@
 'use strict';
 angular.module('ngNumeraljs', [])
     .filter('numeraljs', ['$sce', function ($sce) {
-        return function (input, format) {
+        return function (input, format, noZerosTrimming) {
             if (input == null || format == null) 
                 return input;
             if (format === '') 
@@ -10,8 +10,13 @@ angular.module('ngNumeraljs', [])
             if (format.indexOf('.') < 0) 
                 format = format.replace(/(\D*$)/, ".0$1");
 
-            var nums = numeral(input).format(format).replace(/0+(?=\D+$)|0+$/g, '').replace(/(^\D*|:)?0+/g, function(str, p1) {return p1 ? p1 + '0' : str;}).split('.');
-            if (nums.length > 1 && parseInt(nums[1].replace('/\D/g', '')) > 0) {
+            var nums = numeral(input).format(format);
+            if (!noZerosTrimming) {
+                nums = nums.replace(/0+(?=\D+$)|0+$/g, '').replace(/(^\D*|:)?0+/g, function(str, p1) {return p1 ? p1 + '0' : str;});
+            }
+            nums = nums.split('.');
+
+            if (noZerosTrimming || (nums.length > 1 && parseInt(nums[1].replace('/\D/g', '')) > 0)) {
                 nums[0] = '<span class="numeral-left">' + nums[0] + '</span>';
             	nums[1] = '<span class="numeral-right">.' + nums[1] + '</span>';
             }
