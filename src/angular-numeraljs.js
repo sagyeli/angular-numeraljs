9a1c67546259880cdbbf7aa2312adf87a4a3b1c0
Eli Sagy <sagyeli@gmail.com>
2014-07-28 02:06:44 +0200

    Cleaning up bugged code

--- src/angular-numeraljs.js
+++ src/angular-numeraljs.js
@@ -7,26 +7,20 @@ angular.module('ngNumeraljs', [])
                 return input;
             if (format === '') 
                 return '';
-            if (format.indexOf('.') < 0) 
-                format = format.replace(/(\D*$)/, ".0$1");
 
             var nums = numeral(input).format(format);
-            if (!noZerosTrimming) {
+            if (!noZerosTrimming && nums !== '0') {
                 nums = nums.replace(/0+(?=\D+$)|0+$/g, '').replace(/(^\D*|:)?0+/g, function(str, p1) {return p1 ? p1 + '0' : str;});
             }
             nums = nums.split('.');
 
-            if (nums[1] && format.indexOf('.') >= 0 && nums[1].replace(/\D/g, '') === '') {
+            if (nums[1] !== undefined && format.indexOf('.') >= 0 && nums[1].replace(/\D/g, '') === '') {
                 nums[1] = '0' + nums[1];
             }
 
-            if (noZerosTrimming || (nums.length > 1 && parseInt(nums[1].replace(/\D/g, '')) > 0)) {
-		if (nums[1].replace('/\D/g', '') === '') {
-			nums[1] = '0' + nums[1];
-		}
-
+            if (nums.length > 1) {
                 nums[0] = '<span class="numeral-left">' + nums[0] + '</span>';
-            	nums[1] = '<span class="numeral-right">.' + nums[1] + '</span>';
+		nums[1] = '<span class="numeral-right">.' + nums[1] + '</span>';
             }
             else {
                 nums[0] = '<span class="numeral-integer">' + nums[0] + '</span>';
