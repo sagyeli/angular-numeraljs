76b93e9de5e895b3e278c50f54777ae16448fb75
Eli Sagy <sagyeli@gmail.com>
2014-07-29 23:53:36 +0300

    Minor bug fix

--- src/angular-numeraljs.js
+++ src/angular-numeraljs.js
@@ -3,10 +3,16 @@
 angular.module('ngNumeraljs', [])
     .filter('numeraljs', ['$sce', function ($sce) {
         return function (input, format, noZerosTrimming) {
+            var dropFraction = false;
+
             if (input == null || format == null) 
                 return input;
             if (format === '') 
                 return '';
+            if (format.indexOf('.') < 0) {
+                format = format.replace(/(\D*$)/, ".0$1");
+                dropFraction = true;
+            }
 
             var nums = numeral(input).format(format);
             if (!noZerosTrimming && nums !== '0') {
@@ -18,9 +24,9 @@ angular.module('ngNumeraljs', [])
                 nums[1] = '0' + nums[1];
             }
 
-            if (nums.length > 1) {
+            if (nums.length > 1 && !dropFraction) {
                 nums[0] = '<span class="numeral-left">' + nums[0] + '</span>';
-		nums[1] = '<span class="numeral-right">.' + nums[1] + '</span>';
+                nums[1] = '<span class="numeral-right">.' + nums[1] + '</span>';
             }
             else {
                 nums[0] = '<span class="numeral-integer">' + nums[0] + '</span>';
